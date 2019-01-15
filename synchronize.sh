f8cab71c7ffb8aaf38c719df5a7f1d2649a038cf
Veziik <menendez.daniel2@gmail.com>
2019-01-14 22:14:32 -0500

    HOMEBASEREMADE commit Mon Jan 14 22:14:32 STD 2019

--- synchronize.sh
+++ synchronize.sh
@@ -3,7 +3,7 @@ NAME=`hostname`
 DATE=`date`
 COMMIT=" commit " 
 FINALSTR=$NAME$COMMIT$DATE 
-echo "starting program commit from FINALSTR"
+echo "starting program commit from $FINALSTR"
 git add . 
 git commit -m "$FINALSTR"
 git push origin master

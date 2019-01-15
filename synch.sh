ab36fc3ea69df0e4e9f04fa24b1930a55efa3436
Veziik <menendez.daniel2@gmail.com>
2019-01-14 22:51:30 -0500

    chrx commit Mon Jan 14 22:51:30 EST 2019

--- synch.sh
+++ synch.sh
@@ -1,9 +1,11 @@
 #! /bin/bash 
+NAME=`hostname` 
+DATE=`date`
 
 printf "starting program pull from git...\n\n"
-git pull origin master
+git pull origin master -m "pull from $NAME on $DATE"
 
 printf "starting file pull from git...\n\n"
 cd files
-git pull origin master
+git pull origin master -m "pull from $NAME on $DATE"
 cd ../

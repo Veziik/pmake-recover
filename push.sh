8c1afde476223143d8f55cea6a3ec2bee71d2a9e
Veziik <menendez.daniel2@gmail.com>
2019-01-14 22:17:52 -0500

    HOMEBASEREMADE commit Mon Jan 14 22:17:52 STD 2019

---
+++ push.sh
@@ -0,0 +1,18 @@
+#! /bin/bash 
+NAME=`hostname` 
+DATE=`date`
+COMMIT=" commit " 
+FINALSTR=$NAME$COMMIT$DATE 
+
+printf "starting program commit from $NAME on $DATE\n\n"
+git add . 
+git commit -m "$FINALSTR"
+git push origin master
+
+
+printf "starting file commit from $NAME on $DATE\n\n"
+cd files
+git add . 
+git commit -m "$FINALSTR"
+git push origin master
+cd ../

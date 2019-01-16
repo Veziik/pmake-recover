b52405d43b3cb4125f3fb2db3f9b8e64c0bc2afb
Veziik <menendez.daniel2@gmail.com>
2019-01-16 01:42:53 -0500

    HOMEBASEREMADE commit Wed Jan 16 01:42:53 STD 2019

--- push.sh
+++ push.sh
@@ -12,13 +12,13 @@ fi
 FINALSTR=$NAME$COMMIT$DATE$MESSAGE
 
 
-printf "\nstarting program commit and push from $NAME on $DATE..."
+printf "\nstarting program commit and push from $NAME on $DATE...\n"
 git add --all
 git commit -m "$FINALSTR"
 git push origin master
 printf "\n"
 
-printf "\nstarting file commit and push from $NAME on $DATE..."
+printf "\nstarting file commit and push from $NAME on $DATE...\n"
 cd files
 git add --all
 git commit -m "$FINALSTR"

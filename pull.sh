85ae6e70dded3995099394a1901559a1f5c168e7
Veziik <menendez.daniel2@gmail.com>
2019-01-16 14:41:24 -0500

    HOMEBASEREMADE commit Wed Jan 16 14:41:24 STD 2019

--- pull.sh
+++ pull.sh
@@ -1,11 +1,9 @@
 #! /bin/bash 
-#NAME=`hostname` 
-#DATE=`date`
 
-printf "starting program pull from git...\n\n"
+printf "\nstarting program pull from git...\n"
 git pull origin master #-m "pull from $NAME on $DATE"
 
-printf "starting file pull from git...\n\n"
+printf "\nstarting file pull from git...\n"
 cd files
 git pull origin master #-m "pull from $NAME on $DATE"
 cd ../

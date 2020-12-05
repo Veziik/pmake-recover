99603982ac38b7769c404c209fb3affd910e6254
RemoteBase <menendez.daniel2@gmail.com>
2020-12-05 04:59:37 +0000

    chrx commit Sat Dec  5 04:59:37 UTC 2020: potato

--- push.sh
+++ push.sh
@@ -2,7 +2,8 @@
 NAME=`hostname` 
 DATE=`date`
 COMMIT=" commit " 
-MESSAGE=""
+MESSAGE1=""
+MESSAGE2=""
 
 if [ $# -eq 1 ]
 then
@@ -12,6 +13,8 @@ fi
 if [ $# -eq 2 ]
 then
 	MESSAGE2=": $2"
+else
+	MESSAGE2=": $1"
 fi
 
 FINALSTR1=$NAME$COMMIT$DATE$MESSAGE1
